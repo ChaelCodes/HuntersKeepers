46d6b484645131de3b652ccdea1f052a2c92f78c
JHylton <jessica.hylton42@gmail.com>
2019-09-01 21:12:23 -0400

    Validate Harm and Luck between 0 and 7

--- app/models/hunter.rb
+++ app/models/hunter.rb
@@ -1,2 +1,6 @@
 class Hunter < ApplicationRecord
+
+  validates :harm, numericality: { less_than_or_equal_to: 7, greater_than_or_equal_to: 0 }
+  validates :luck, numericality: { less_than_or_equal_to: 7, greater_than_or_equal_to: 0 }
+
 end
