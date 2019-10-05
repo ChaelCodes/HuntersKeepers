2ccbae419938602949ea220a7b6e3abec3da8920
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2019-10-05 12:46:46 -0400

    Valid specs for Hunter

--- spec/views/hunters/index.html.erb_spec.rb
+++ spec/views/hunters/index.html.erb_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe "hunters/index", type: :view do
   before(:each) do
     assign(:hunters, [
-      Hunter.create!(),
-      Hunter.create!()
+      Hunter.create!(playbook: Playbook.create, harm: 0, luck: 7),
+      Hunter.create!(playbook: Playbook.create, harm: 0, luck: 7)
     ])
   end
 
--- spec/views/hunters/show.html.erb_spec.rb
+++ spec/views/hunters/show.html.erb_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "hunters/show", type: :view do
   before(:each) do
-    @hunter = assign(:hunter, Hunter.create!())
+    @hunter = assign(:hunter, Hunter.create!(playbook: Playbook.create, harm: 0, luck: 7))
   end
 
   it "renders attributes in <p>" do
