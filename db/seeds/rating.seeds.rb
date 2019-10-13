10ec7d4556b91dd4cbc2dd8ed09fbf1094b7c5b2
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2019-10-13 11:36:36 -0400

    Add Seedbank, seeds files for Playbooks, Ratings, Moves, and also Hunters, which are only in development.

---
+++ db/seeds/rating.seeds.rb
@@ -0,0 +1,89 @@
+{ chosen: [
+  { playbook_id: 1, charm: 2, cool:-1, sharp: 1, tough: 2, weird:-1},
+  { playbook_id: 1, charm:-1, cool: 2, sharp: 1, tough: 2, weird:-1},
+  { playbook_id: 1, charm: 1, cool: 2, sharp: 1, tough: 1, weird:-1},
+  { playbook_id: 1, charm:-1, cool: 1, sharp: 2, tough:-1, weird: 2},
+  { playbook_id: 1, charm: 1, cool: 2, sharp:-1, tough:-1, weird: 2},
+],
+crooked: [
+  { playbook_id: 2, charm: 1, cool: 1, sharp: 2, tough: 0, weird:-1},
+  { playbook_id: 2, charm:-1, cool: 1, sharp: 1, tough: 2, weird: 0},
+  { playbook_id: 2, charm:-1, cool: 2, sharp: 2, tough: 0, weird:-1},
+  { playbook_id: 2, charm: 2, cool: 1, sharp: 1, tough: 0, weird:-1},
+  { playbook_id: 2, charm: 2, cool: 0, sharp: 1, tough:-1, weird: 1},
+],
+divine: [
+  { playbook_id: 3, charm: 1, cool: 1, sharp:-1, tough: 2, weird: 0},
+  { playbook_id: 3, charm:-1, cool: 2, sharp:-1, tough: 2, weird: 0},
+  { playbook_id: 3, charm:-1, cool: 0, sharp: 1, tough: 2, weird: 1},
+  { playbook_id: 3, charm: 1, cool: 1, sharp: 0, tough: 2, weird:-1},
+  { playbook_id: 3, charm:-1, cool: 1, sharp: 0, tough: 2, weird: 1},
+],
+expert: [
+  { playbook_id: 4, charm:-1, cool: 1, sharp: 2, tough: 1, weird: 0},
+  { playbook_id: 4, charm: 0, cool: 1, sharp: 2, tough:-1, weird: 1},
+  { playbook_id: 4, charm: 1, cool:-1, sharp: 2, tough: 1, weird: 0},
+  { playbook_id: 4, charm:-1, cool: 1, sharp: 2, tough: 0, weird: 1},
+  { playbook_id: 4, charm:-1, cool: 0, sharp: 2, tough:-1, weird: 2},
+],
+flake: [
+  { playbook_id: 5, charm: 1, cool: 1, sharp: 2, tough:-1, weird: 0},
+  { playbook_id: 5, charm: 0, cool: 1, sharp: 2, tough:-1, weird: 1},
+  { playbook_id: 5, charm: 1, cool:-1, sharp: 2, tough: 1, weird: 0},
+  { playbook_id: 5, charm: 1, cool:-1, sharp: 2, tough: 0, weird: 1},
+  { playbook_id: 5, charm:-1, cool:-1, sharp: 2, tough: 0, weird: 2},
+],
+initiate: [
+  { playbook_id: 6, charm:-1, cool: 1, sharp: 0, tough: 1, weird: 2},
+  { playbook_id: 6, charm: 0, cool: 1, sharp: 1, tough:-1, weird: 2},
+  { playbook_id: 6, charm:-1, cool: 0, sharp:-1, tough: 2, weird: 2},
+  { playbook_id: 6, charm: 1, cool:-1, sharp: 1, tough: 0, weird: 2},
+  { playbook_id: 6, charm: 0, cool: 0, sharp: 0, tough: 1, weird: 2},
+],
+monstruous: [
+  { playbook_id: 7, charm:-1, cool:-1, sharp: 0, tough: 2, weird: 3},
+  { playbook_id: 7, charm:-1, cool: 1, sharp: 1, tough: 0, weird: 3},
+  { playbook_id: 7, charm: 2, cool: 0, sharp:-1, tough:-1, weird: 3},
+  { playbook_id: 7, charm:-2, cool: 2, sharp: 0, tough: 0, weird: 3},
+  { playbook_id: 7, charm: 0, cool:-1, sharp: 2, tough:-1, weird: 3},
+],
+mundane: [
+  { playbook_id: 8, charm: 2, cool: 1, sharp: 0, tough: 1, weird:-1},
+  { playbook_id: 8, charm: 2, cool:-1, sharp: 1, tough: 1, weird: 0},
+  { playbook_id: 8, charm: 2, cool: 0, sharp:-1, tough: 1, weird: 1},
+  { playbook_id: 8, charm: 2, cool: 0, sharp: 1, tough: 1, weird:-1},
+  { playbook_id: 8, charm: 2, cool: 1, sharp: 1, tough: 0, weird:-1},
+],
+professional: [
+  { playbook_id: 9, charm: 0, cool: 2, sharp:-1, tough: 2, weird:-1},
+  { playbook_id: 9, charm:-1, cool: 2, sharp: 1, tough: 1, weird: 0},
+  { playbook_id: 9, charm: 1, cool: 2, sharp: 1, tough:-1, weird: 0},
+  { playbook_id: 9, charm:-1, cool: 2, sharp: 1, tough: 0, weird: 1},
+  { playbook_id: 9, charm: 0, cool: 2, sharp: 2, tough:-1, weird:-1},
+],
+spell_slinger: [
+  { playbook_id: 10, charm:-1, cool: 1, sharp: 1, tough: 0, weird: 2},
+  { playbook_id: 10, charm: 0, cool:-1, sharp: 1, tough: 1, weird: 2},
+  { playbook_id: 10, charm:-1, cool: 0, sharp: 2, tough:-1, weird: 2},
+  { playbook_id: 10, charm: 1, cool: 0, sharp: 1, tough:-1, weird: 2},
+  { playbook_id: 10, charm: 0, cool: 0, sharp: 1, tough: 0, weird: 2},
+],
+spooky: [
+  { playbook_id: 11, charm: 1, cool: 0, sharp: 1, tough:-1, weird: 2},
+  { playbook_id: 11, charm:-1, cool: 1, sharp: 0, tough: 1, weird: 2},
+  { playbook_id: 11, charm: 2, cool: 0, sharp:-1, tough:-1, weird: 2},
+  { playbook_id: 11, charm: 0, cool:-1, sharp: 1, tough: 1, weird: 2},
+  { playbook_id: 11, charm:-1, cool:-1, sharp: 2, tough: 0, weird: 2},
+],
+wronged: [
+  { playbook_id: 12, charm: 0, cool: 1, sharp:-1, tough: 2, weird: 1},
+  { playbook_id: 12, charm: 0, cool: 0, sharp: 1, tough: 2, weird: 0},
+  { playbook_id: 12, charm: 1, cool: 0, sharp: 1, tough: 2, weird:-1},
+  { playbook_id: 12, charm:-1, cool:-1, sharp: 0, tough: 2, weird: 2},
+  { playbook_id: 12, charm: 1, cool:-1, sharp: 0, tough: 2, weird: 1},
+]}.each do |playbook_id, ratings|
+  ratings.each do |rating|
+    Rating.find_or_create_by(rating)
+  end
+end
+
