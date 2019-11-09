1a0f3661cad3355a8c13f2d3122996374e0c0c15
JHylton <jessica.hylton42@gmail.com>
2019-11-09 13:03:47 -0500

    Code climate changes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Base controller, all other controllers inherit.
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Represent the user and interface with Devise to log into the application.
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
