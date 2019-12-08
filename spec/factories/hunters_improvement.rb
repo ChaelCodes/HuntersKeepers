d9b0cacacba0b440df829eb20aabc4b81c427184
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2019-12-07 20:53:36 -0500

    Fix HuntersImprovementFactory

--- spec/factories/hunters_improvement.rb
+++ spec/factories/hunters_improvement.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  factory :same_playbook, class: 'Playbook' do
+  end
+  playbook = FactoryBot.create(:same_playbook)
+
   factory :hunters_improvement do
-    hunter
-    improvement
+    association :hunter, playbook: playbook
+    association :improvement, playbook: playbook
   end
 end
