8c7d8d3fdf1c5bc1532b1e9ce3912bd514f38206
JHylton <jessica.hylton42@gmail.com>
2019-12-17 19:33:58 -0500

    Added Pundit gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 # Base controller, all other controllers inherit.
+# Pundit controller for application policy
 class ApplicationController < ActionController::Base
+  include Pundit
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 end
+
