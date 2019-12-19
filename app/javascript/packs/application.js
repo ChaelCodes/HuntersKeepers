0000170f1a9e5773ec7352d70cc73d94c88aea1f
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2019-12-19 10:44:31 -0500

    Basic app working - css blobs permitted, and vue component shows on page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,3 +8,20 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import TurbolinksAdapter from 'vue-turbolinks'
+import Vue from 'vue/dist/vue.esm'
+import App from '../app.vue'
+
+Vue.use(TurbolinksAdapter)
+
+document.addEventListener('turbolinks:load', () => {
+  const app = new Vue({
+    el: '#hello',
+    data: () => {
+      return {
+        message: "Can you say hello?"
+      }
+    },
+    components: { App }
+  })
+})
--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -12,10 +12,11 @@ Rails.application.config.content_security_policy do |policy|
   policy.object_src  :none
   if Rails.env.development?
     policy.script_src :self, :https, :unsafe_eval
+    policy.style_src   :self, :https, :blob, :unsafe_eval
   else
     policy.script_src :self, :https
+    policy.style_src   :self, :https
   end
-  policy.style_src   :self, :https
   # If you are using webpack-dev-server then specify webpack-dev-server host
   policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
 
