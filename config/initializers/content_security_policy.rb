2fc0140c164c05c6bde06d84e3b4820e1ad90e4c
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2020-01-19 12:11:09 -0500

    relax csp

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -14,8 +14,8 @@ Rails.application.config.content_security_policy do |policy|
     policy.script_src :self, :https, :unsafe_eval
     policy.style_src   :self, :https, :blob, :unsafe_eval
   else
-    policy.script_src :self, :https
-    policy.style_src   :self, :https
+    policy.script_src :self, :https, :unsafe_eval
+    policy.style_src   :self, :https, :blob, :unsafe_eval
   end
   # If you are using webpack-dev-server then specify webpack-dev-server host
   policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
