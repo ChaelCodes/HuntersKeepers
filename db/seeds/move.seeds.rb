8002888827fa893068b06a25980d350d87d7de25
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2020-04-05 10:53:32 -0400

    Rubocop fixes to seedfiles

--- db/seeds/move.seeds.rb
+++ db/seeds/move.seeds.rb
@@ -23,7 +23,7 @@ do it).',
 they do what you want right now,
 they also become your ally for the
 rest of the mystery (or, if you do
-enough for them, permanently).',
+enough for them, permanently).'
 },
  {
    name: 'Act Under Pressure',
@@ -53,11 +53,11 @@ to trouble or danger.',
 to their roll.',
    twelve_plus: 'On a 12+ your help
 lets them act as if they just rolled a 12,
-regardless of what they actually got.',
-},
-{
-  name: 'Investigate a Mystery',
-  description: 'When you investigate a mystery, roll
+regardless of what they actually got.'
+ },
+ {
+   name: 'Investigate a Mystery',
+   description: 'When you investigate a mystery, roll
 +Sharp. On a 10+ hold 2, and on a 7-9
 hold 1. One hold can be spent to ask the
 Keeper one of the following questions:
@@ -68,9 +68,9 @@ Keeper one of the following questions:
 • Where did it go?
 • What was it going to do?
 • What is being concealed here?',
-  rating: 'sharp',
-  six_and_under: "Failure. Keeper's choice.",
-  seven_to_nine: 'You can ask the
+   rating: 'sharp',
+   six_and_under: "Failure. Keeper's choice.",
+   seven_to_nine: 'You can ask the
 Keeper one of the following questions:
 • What happened here?
 • What sort of creature is it?
@@ -273,66 +273,66 @@ with.
 • Observe another place or time.
 • Heal 1-harm from an injury, or
 cure a disease, or neutralize a
-poison.',
-},
-{
-  name: 'Alt: Empath',
-  rating:'weird',
-  six_and_under: 'Your brain is overwhelmed with emotion.',
-  seven_to_nine: 'You gain a hazy impression of their current emotional
+poison.'
+ },
+ {
+   name: 'Alt: Empath',
+   rating: 'weird',
+   six_and_under: 'Your brain is overwhelmed with emotion.',
+   seven_to_nine: 'You gain a hazy impression of their current emotional
 state and intentions.',
-  ten_plus: 'You gain a clear
+   ten_plus: 'You gain a clear
 impression of their current
 emotional state and intentions.
 Take +1 forward when acting on
 this knowledge.',
-  twelve_plus: 'You get an impression
+   twelve_plus: 'You get an impression
 (as for 10 or more), and
 you may ask one follow-up question that the Keeper will answer
-honestly.',
-},
-{
-  name: 'Alt: Illuminated',
-  rating: 'weird',
-  six_and_under: 'The Secret Masters’
+honestly.'
+ },
+ {
+   name: 'Alt: Illuminated',
+   rating: 'weird',
+   six_and_under: 'The Secret Masters’
 reply is terrible, garbled, or
 somehow dangerously wrong.',
-  seven_to_nine: 'The Secret Masters need
+   seven_to_nine: 'The Secret Masters need
 you to complete a task for them.
 Once it is done, they reveal a key
 fact, clue, or technique that will
 help you.',
-  ten_plus: 'The Secret
+   ten_plus: 'The Secret
 Masters reveal a key fact, clue, or
 technique that will help you.',
-  twelve_plus: 'The Secret
+   twelve_plus: 'The Secret
 Masters reveal a key fact, clue,
 or technique that will help you.
 You may ask one follow-up question that the Keeper will answer
-honestly.',
-},
-{
-  name: 'Alt: No Limits',
-  rating: 'weird',
-  six_and_under: 'Something goes
+honestly.'
+ },
+ {
+   name: 'Alt: No Limits',
+   rating: 'weird',
+   six_and_under: 'Something goes
 horribly wrong.',
-  seven_to_nine: 'You do it but choose
+   seven_to_nine: 'You do it but choose
 one consequence: suffer 1-harm,
 take –1 forward, or you need to
 rest right now',
-  ten_plus: 'Your body obeys
+   ten_plus: 'Your body obeys
 your will, to the limits of physical possibility (see below), for a
 moment.',
-  twelve_plus: 'You can continue
+   twelve_plus: 'You can continue
 acting at your body’s limits for 30
-seconds.',
-},
-{
-  name: 'Alt: Past Lives',
-  rating: 'weird',
-  six_and_under: 'A past life takes over
+seconds.'
+ },
+ {
+   name: 'Alt: Past Lives',
+   rating: 'weird',
+   six_and_under: 'A past life takes over
 for a while.',
-  seven_to_nine: 'A past life has a little
+   seven_to_nine: 'A past life has a little
 experience with this. Ask the
 Keeper one of the questions below
 • What did a past life discover about
@@ -345,7 +345,7 @@ a past life show me the way to?
 to help them?
 • What does a past life advise me to
 do now?',
-  ten_plus: 'A past life has
+   ten_plus: 'A past life has
 something useful to offer. Ask the
 Keeper two of the questions below.
 • What did a past life discover about
@@ -358,7 +358,7 @@ a past life show me the way to?
 to help them?
 • What does a past life advise me to
 do now?',
-  twelve_plus: 'A past life knows
+   twelve_plus: 'A past life knows
 exactly what you were after. Ask
 the Keeper one of the questions
 below, and one free-form question.
@@ -373,45 +373,45 @@ a past life show me the way to?
 • What did a past life learn too late
 to help them?
 • What does a past life advise me to
-do now?',
-},
-{
-  name: 'Alt: Sentive',
-  rating: 'weird',
-  six_and_under: 'Your brain makes
+do now?'
+ },
+ {
+   name: 'Alt: Sentive',
+   rating: 'weird',
+   six_and_under: 'Your brain makes
 contact with something dangerous.',
-  seven_to_nine: 'You gain a hazy impression about something important.',
-  ten_plus: 'You gain a definite impression (a vision, tangible
+   seven_to_nine: 'You gain a hazy impression about something important.',
+   ten_plus: 'You gain a definite impression (a vision, tangible
 aura, overheard thought, etc) about
 something important.',
-  twelve_plus: 'You get an
+   twelve_plus: 'You get an
 impression as for 10 or more, plus
 you may ask one follow-up question that the Keeper will answer
-honestly.',
-},
-{
-  name: 'Alt: Trust Your Gut',
-  rating: 'weird',
-  six_and_under: 'Your instincts lead you
+honestly.'
+ },
+ {
+   name: 'Alt: Trust Your Gut',
+   rating: 'weird',
+   six_and_under: 'Your instincts lead you
 into danger',
-  seven_to_nine: 'The Keeper will tell you
+   seven_to_nine: 'The Keeper will tell you
 a general direction to go. Take +1
 forward as you explore that.',
-  ten_plus: 'The Keeper will
+   ten_plus: 'The Keeper will
 tell where you should go. Wherever that is, it will be important.
 You get +1 ongoing on the way to
 this place.',
-  twelve_plus: 'In addition to the
+   twelve_plus: 'In addition to the
 usual 10+ result, the Keeper will
 tell you about one important thing
-you should investigate further.',
-},
-{
-  name: 'Alt: Telekinesis',
-  rating: 'weird',
-  six_and_under: 'Something goes
+you should investigate further.'
+ },
+ {
+   name: 'Alt: Telekinesis',
+   rating: 'weird',
+   six_and_under: 'Something goes
 horribly wrong.',
-  seven_to_nine: 'You move it but it hurts.
+   seven_to_nine: 'You move it but it hurts.
 Choose one option and mark
 2-harm.
 • Something is held fast.
@@ -422,7 +422,7 @@ than a person.
 • You keep it basically under your
 control.
 • You suffer 1 less harm.',
-  ten_plus: 'You move it.
+   ten_plus: 'You move it.
 Choose two options and mark
 1-harm.
 • Something is held fast.
@@ -433,7 +433,7 @@ than a person.
 • You keep it basically under your
 control.
 • You suffer 1 less harm.',
-  twelve_plus: 'Choose three
+   twelve_plus: 'Choose three
 options. You may also choose from
 the advanced options:
 Normal Options:
@@ -454,12 +454,12 @@ close crush)
 control.
 • You have perfect and precise
 control over exactly what
-happens.',
-},
-{
-  name: 'Alt: Weird Science',
-  rating: 'weird',
-  six_and_under: 'Something goes
+happens.'
+ },
+ {
+   name: 'Alt: Weird Science',
+   rating: 'weird',
+   six_and_under: 'Something goes
 horribly wrong. You are still able to
 create your device, but the Keeper
 picks three requirements.
@@ -473,7 +473,7 @@ working.
 intended.
 • You’ll need help (beyond the
 hunters on your team) to finish it.',
-  seven_to_nine: 'you pick one requirement and the Keeper picks a
+   seven_to_nine: 'you pick one requirement and the Keeper picks a
 second one.
 It needs a rare and/or weird material.
 • It won’t be very reliable.
@@ -485,7 +485,7 @@ working.
 intended.
 • You’ll need help (beyond the
 hunters on your team) to finish it.',
-  ten_plus: 'You pick two
+   ten_plus: 'You pick two
 requirements.
 • It needs a rare and/or weird material.
 • It won’t be very reliable.
@@ -497,7 +497,7 @@ working.
 intended.
 • You’ll need help (beyond the
 hunters on your team) to finish it.',
-  twelve_plus: 'On a 12 or more, you gain +1
+   twelve_plus: 'On a 12 or more, you gain +1
 ongoing when operating the device.
 • It needs a rare and/or weird material.
 • It won’t be very reliable.
@@ -508,8 +508,8 @@ working.
 • It won’t work exactly as you
 intended.
 • You’ll need help (beyond the
-hunters on your team) to finish it.',
-}].each do |move|
+hunters on your team) to finish it.'
+ }].each do |move|
   Moves::Basic.find_or_create_by(move)
 end
 
@@ -545,25 +545,25 @@ after :playbook do
       a 10+ they provide some useful info or help in the
       field.'
   },
-  {
-    name: 'Ancient Fighting Arts',
-    type: 'Moves::Descriptive',
-    playbook_id: @initiate.id,
-    description: 'When using an old-fashioned hand weapon, you inflict +1 harm and get +1
+   {
+     name: 'Ancient Fighting Arts',
+     type: 'Moves::Descriptive',
+     playbook_id: @initiate.id,
+     description: 'When using an old-fashioned hand weapon, you inflict +1 harm and get +1
 whenever you roll protect someone.'
-  },
-  {
-    name: 'Mystic',
-    type: 'Moves::Descriptive',
-    playbook_id: @initiate.id,
-    description: 'Every time you successfully use magic, take
+   },
+   {
+     name: 'Mystic',
+     type: 'Moves::Descriptive',
+     playbook_id: @initiate.id,
+     description: 'Every time you successfully use magic, take
 +1 forward.'
-  },
-  {
-    name: 'Fortunes',
-    type: 'Moves::Rollable',
-    playbook_id: @initiate.id,
-    description: 'The Sect has ancient prophecies or divination techniques to predict the future. Once per
+   },
+   {
+     name: 'Fortunes',
+     type: 'Moves::Rollable',
+     playbook_id: @initiate.id,
+     description: 'The Sect has ancient prophecies or divination techniques to predict the future. Once per
       mystery, you may use them. If you look at what the
       future holds, roll +Weird. On a 10+ hold 3, and on
       a 7-9 hold 1. On a miss, you get bad information and
@@ -575,10 +575,10 @@ whenever you roll protect someone.'
       hunter.
       • retroactively warn someone about an attack, so
       that it doesn’t happen.',
-    rating: :weird,
-    six_and_under: 'On a miss, you get bad information and
+     rating: :weird,
+     six_and_under: 'On a miss, you get bad information and
       the Keeper decides how that affects you.',
-    seven_to_nine: 'Spend your 1
+     seven_to_nine: 'Spend your 1
       hold to:
       • have a useful object ready.
       • be somewhere you are needed, just in time.
@@ -586,7 +586,7 @@ whenever you roll protect someone.'
       hunter.
       • retroactively warn someone about an attack, so
       that it doesn’t happen.',
-    ten_plus: 'Spend your 3
+     ten_plus: 'Spend your 3
       hold to:
       • have a useful object ready.
       • be somewhere you are needed, just in time.
@@ -594,12 +594,12 @@ whenever you roll protect someone.'
       hunter.
       • retroactively warn someone about an attack, so
       that it doesn’t happen.'
-  },
-  {
-    name: 'Sacred Oath',
-    type: 'Moves::Descriptive',
-    playbook_id: @initiate.id,
-    description: ' You may bind yourself to a single goal,
+   },
+   {
+     name: 'Sacred Oath',
+     type: 'Moves::Descriptive',
+     playbook_id: @initiate.id,
+     description: ' You may bind yourself to a single goal,
 forsaking something during your quest (e.g. speech,
 all sustenance but bread and water, alcohol, lying,
 sex, etc). Get the Keeper’s agreement on this—it
@@ -609,49 +609,49 @@ goal, mark experience at the end of every session
 and get +1 on any rolls that directly help achieve the
 goal. If you break the oath, take -1 ongoing until you
 have atoned.'
-  },
-  {
-    name: 'Mentor',
-    type: 'Moves::Rollable',
-    playbook_id: @initiate.id,
-    description: 'You have a mentor in the Sect: name
+   },
+   {
+     name: 'Mentor',
+     type: 'Moves::Rollable',
+     playbook_id: @initiate.id,
+     description: 'You have a mentor in the Sect: name
       them. When you contact your mentor for info, roll
       +Sharp. On a 10+, you get an answer to your question, no problem. On a 7-9 you choose: they’re either
       busy and can’t help, or they answer the question but
       you owe a favour. On a miss, your question causes
       trouble.',
-    rating: :sharp,
-    six_and_under: 'On a miss, your question causes
+     rating: :sharp,
+     six_and_under: 'On a miss, your question causes
       trouble.',
-    seven_to_nine: 'On a 7-9 you choose: they’re either
+     seven_to_nine: 'On a 7-9 you choose: they’re either
       busy and can’t help, or they answer the question but
       you owe a favour.',
-    ten_plus: 'On a 10+, you get an answer to your question, no problem.'
-  },
-  {
-    name: 'Apprentice',
-    type: 'Moves::Descriptive',
-    playbook_id: @initiate.id,
-    description: 'You have an apprentice: name them.
+     ten_plus: 'On a 10+, you get an answer to your question, no problem.'
+   },
+   {
+     name: 'Apprentice',
+     type: 'Moves::Descriptive',
+     playbook_id: @initiate.id,
+     description: 'You have an apprentice: name them.
       Your job is to teach them the Sect’s ways. They count
       as an ally: subordinate (motivation: to follow your
       instructions to the letter).'
-  },
-  {
-    name: 'Helping Hand',
-    type: 'Moves::Descriptive',
-    playbook_id: @initiate.id,
-    description: 'When you successfully help out
+   },
+   {
+     name: 'Helping Hand',
+     type: 'Moves::Descriptive',
+     playbook_id: @initiate.id,
+     description: 'When you successfully help out
       another hunter, they get +2 instead of the usual +1.'
-  },
-  {
-    name: 'That Old Black Magic',
-    type: 'Moves::Descriptive',
-    playbook_id: @initiate.id,
-    description: 'When you use magic, you
+   },
+   {
+     name: 'That Old Black Magic',
+     type: 'Moves::Descriptive',
+     playbook_id: @initiate.id,
+     description: 'When you use magic, you
       can ask a question from the investigate a mystery
       move as your effect.'
-  }].each do |move|
+   }].each do |move|
     Move.find_or_create_by(move)
   end
 end
