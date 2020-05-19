852a578d0f1fee64eed177b9bacc95c07f941caf
Tripp Martin <tripp.martin3@gmail.com>
2020-05-19 11:40:09 -0400

    Reverted spelling for 'referrer.' Configured Rails/RequestReferer in the rubocop.yml file to check for 'referrer'.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,6 @@ class ApplicationController < ActionController::Base
 
   def user_not_authorized
     flash[:alert] = 'You are not authorized to perform this action.'
-    redirect_to(request.referer || root_path)
+    redirect_to(request.referrer || root_path)
   end
 end
