21d8a2a13ff389dbb27138cca2093331babc77d4
Troy Underwood <troyizzle@gmail.com>
2020-06-14 15:59:16 -0400

    137 Add link to playbook Added a link to the playbook. Added a test for the response body to include the path to playbook

--- spec/requests/hunters_spec.rb
+++ spec/requests/hunters_spec.rb
@@ -13,4 +13,15 @@ RSpec.describe 'Hunters', type: :request, js: true do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'GET /hunters/:id' do
+    let!(:hunter) { create(:hunter) }
+    let!(:url) { "/hunters/#{hunter.id}" }
+
+    before { get url }
+
+    it 'contains path to playbook url' do
+      expect(response.body).to include(url)
+    end
+  end
 end
