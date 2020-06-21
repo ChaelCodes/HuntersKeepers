cf96c2170700777696b8650d036c4c61fee6739f
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2020-06-21 12:11:07 -0400

    Add Confirmable and Reconfirmable to Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,9 +18,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable,
-         :registerable, :recoverable, :rememberable,
-         :validatable
+  devise :confirmable, :database_authenticatable,
+         :recoverable, :registerable,
+         :rememberable, :validatable
   validates :email, uniqueness: true
 
   def ban!
--- db/migrate/20200621132924_add_security_to_users.rb
+++ db/migrate/20200621132924_add_security_to_users.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
+
 class AddSecurityToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :banned_at, :datetime
-    add_column :users, :confirmed_at, :datetime
   end
 end
