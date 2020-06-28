1f641615fc80bd3477577de42933251d387ee89c
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2020-06-28 11:37:59 -0400

    Add links to gear

--- app/helpers/gears_helper.rb
+++ app/helpers/gears_helper.rb
@@ -7,6 +7,11 @@ module GearsHelper
     gear_tags << "#{gear.harm}-harm" if gear.harm.to_i.positive?
     gear_tags << "#{gear.armor}-armor" if gear.armor.to_i.positive?
     gear_tags += gear.tag_list
+    return '' if gear_tags.empty?
     "(#{gear_tags.join(' ')})"
+  end
+
+  def link_to_gear(gear)
+    gear ? link_to(gear.name, gear) : ''
   end
 end
