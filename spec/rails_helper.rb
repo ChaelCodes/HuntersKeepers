f310a6fdb63a2b165d184181d651f38ba42548a0
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2020-07-12 13:30:40 -0400

    Break devise configurations out of rails_helper and add a helper method for controller specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,18 +3,21 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
-abort('The Rails environment is running in production mode!') if Rails.env.production?
+if Rails.env.production?
+  abort('The Rails environment is running in production mode!')
+end
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require 'devise'
 # Add coverage report to RSPEC
 require 'simplecov'
 SimpleCov.start
 require "pundit/rspec"
 
-# Add Factories
+# Add auth helpers + devise
+require 'support/devise'
+# Add factories
 require 'support/factory_bot'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -68,8 +71,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
-  config.include Devise::Test::IntegrationHelpers, type: :request
-  config.include Devise::Test::ControllerHelpers, type: :controller
-  config.include Warden::Test::Helpers
 end
