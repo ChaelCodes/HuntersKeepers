d5a117caeeaf551c0012e0491c5dbe99a9163cd9
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2020-09-06 15:15:09 -0400

    Create moveable module to support all improve move services

---
+++ app/services/improves/moveable.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+module Improves
+  # Methods supporting a Move-type Improvable
+  # Supports a single Move or many Moves
+  module Moveable
+    def move
+      @move ||=
+        Move.find_by(id: @hunters_improvement.improvable&.dig('move', 'id'))
+    end
+
+    def moves
+      @moves ||=
+        Move.where(id:
+          @hunters_improvement.improvable&.dig('moves')&.pluck('id'))
+    end
+
+    def valid?
+      super
+      check :invalid_move?, :move, 'not found'
+    end
+
+    # Check if moves are already advanced
+    def already_advanced_moves?
+      @hunter.hunters_moves.where(move: @moves).any?(&:advanced?)
+    end
+
+    # Check if move(s) have been found
+    def invalid_move?
+      @move.nil? && @moves.blank?
+    end
+
+    def hunter_move_exists?
+      return false if invalid_move?
+      return false if @move.type == 'Moves::Basic'
+      @hunter.moves.include?(@move)
+    end
+
+    def not_haven_move?
+      return false if invalid_move?
+      @move.present? && !@move.haven
+    end
+
+    # Checks any moves are not basic
+    # relies on moves_count
+    def not_basic_moves?
+      @moves.where.not(type: 'Moves::Basic').count.positive?
+    end
+
+    def playbook_move_mismatch?
+      return false if invalid_move?
+      !playbook_move_match?
+    end
+
+    def playbook_move_match?
+      return false if invalid_move?
+      @improvement.playbook == @move.playbook
+    end
+  end
+end
