9b9d6690317c52f9299e540b3526e08f2c7d85c2
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2020-09-13 14:52:19 -0400

    Add RDoc to ImproveHunter

--- app/services/improve_hunter.rb
+++ app/services/improve_hunter.rb
@@ -1,11 +1,18 @@
 # frozen_string_literal: true
 
+# This is the base class for improving a Hunter. All
+# services that utilize this class belong in /services/improves
 class ImproveHunter
   def self.for(improvement)
     return ImproveHunter unless improvement.type
     improvement.type.gsub('Improvements', 'Improves').constantize
   end
 
+  # This will initialize @hunters_improvement,
+  # @hunter, @improvement, and @improvable.
+  # Any setup that all improve services need will be
+  # handled here.
+  # @param hunters_improvement
   def initialize(hunters_improvement)
     @hunters_improvement = hunters_improvement
     @improvement = hunters_improvement.improvement
@@ -13,11 +20,22 @@ class ImproveHunter
     @improvable = hunters_improvement.improvable
   end
 
+  # This method is overridden and used to
+  # update and improve the hunter in subclasses
+  # returns false if the hunter/improvement/improvable
+  # combination is invalid
+  # returns a truthy value otherwise
   def improve
     return false unless valid?
     true
   end
 
+  # All improve services should extend this method
+  # to apply their own constraints, and if they extend
+  # the last check should be `@hunters_improvement.errors.none?`
+  # This validations checks the hunter is authorized to use
+  # the improvement, and that the hunter is of sufficient level
+  # for advanced improvements.
   def valid?
     check(:hunter_playbook_mismatch?, :hunter,
           "does not match improvement playbook: #{@improvement.playbook.name}")
