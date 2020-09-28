24bf31667ad28f3d6bbb6bf217a6aed910c571e4
Slava Zharkov <vv.zharkov@jet.su>
2020-09-28 12:58:39 +0300

    Add specs for gear model

--- spec/models/gear_spec.rb
+++ spec/models/gear_spec.rb
@@ -14,5 +14,11 @@
 require 'rails_helper'
 
 RSpec.describe Gear, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:name) }
+
+  describe 'factory is valid' do
+    let(:gear) { build :gear }
+
+    it { expect(gear).to be_valid }
+  end
 end
