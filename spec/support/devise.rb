05fb812207315dc015e7934e9da4162a91158db6
ChaelCodes <chaelcodes@gmail.com>
2020-11-15 10:12:51 -0500

    Add support for Capybara Feature Specs

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -15,5 +15,6 @@ RSpec.configure do |config|
 
   config.include Devise::Test::IntegrationHelpers, type: :request
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::IntegrationHelpers, type: :feature
   config.include Warden::Test::Helpers
 end
