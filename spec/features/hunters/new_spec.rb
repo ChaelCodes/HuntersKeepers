a20d932da4381d5ec8204bce53a8a1538ca4d686
ChaelCodes <chaelcodes@gmail.com>
2020-11-22 12:50:59 -0500

    Update hunter/new spec with attributes

--- spec/features/hunters/new_spec.rb
+++ spec/features/hunters/new_spec.rb
@@ -14,12 +14,24 @@ describe 'hunters#new' do
   it 'creates a hunter', js: true do
     visit '/hunters/new'.dup
     expect(page).to have_content 'New Hunter'
-    fill_in 'Name', with: 'Mary Sue Bazooka'
+    fill_in 'hunter[name]', with: 'Mary Sue Bazooka'
     expect(page).to have_content 'Playbooks'
     select(playbook.name, from: 'hunter[playbook_id]')
     select("Charm: 1, Cool: -1, Sharp: 0, Tough: 2, Weird: -2", from: 'hunter[rating_id]')
     click_button 'Create Hunter'
     expect(page).to have_content('Hunter was successfully created.')
+    expect(Hunter.last).to have_attributes(
+      name: 'Mary Sue Bazooka',
+      playbook_id: playbook.id,
+      charm: 1,
+      cool: -1,
+      sharp: 0,
+      tough: 2,
+      weird: -2,
+      harm: 0,
+      luck: 7,
+      experience: 0
+    )
   end
 
   it 'does not create a hunter', js: true do
