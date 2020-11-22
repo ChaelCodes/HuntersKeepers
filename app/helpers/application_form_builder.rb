9ce4f04dd96b66c52be09c97828b783bea1f69ef
ChaelCodes <chaelcodes@gmail.com>
2020-11-22 12:15:30 -0500

    fixup into ApplicatioFormBuilder

--- app/helpers/application_form_builder.rb
+++ app/helpers/application_form_builder.rb
@@ -1,27 +1,28 @@
 class ApplicationFormBuilder < ActionView::Helpers::FormBuilder
   delegate :tag, to: :@template
 
-  def labelled_text_field(method, label_options: {}, input_options: {})
+  def labelled_check_box(method, *input_options, label_options: {})
     tag.div(class: 'field') do
-      label(method, label_options) +
-      tag.div(class: 'control') do
-        text_field(method, input_options)
-      end
+      check_box(method, *input_options) +
+      label(method, label_options)
     end
   end
 
-  def labelled_collection_select(method, label_options: {}, input_options: {})
-    tag.div(class: 'field') do
-      label(method, label_options) +
-      tag.div(class: 'control') do
-        # 😳 @object.association(method.to_s.sub(/_id$/, '')).klass.all
-        collection_select(method,
-          input_options[:collection], 
-          input_options[:value_method] || :id,
-          input_options[:text_method] || :to_s,
-          input_options[:options],
-          input_options[:html_options])
-      end
+  def labelled_collection_select(method, *input_options, label_options: {})
+    form_controls(method, label_options) do
+      collection_select(method, *input_options)
+    end
+  end
+
+  def labelled_number_field(method, label_options: {}, input_options: {})
+    form_controls(method, label_options) do
+      number_field(method, input_options)
+    end
+  end
+
+  def labelled_text_field(method, label_options: {}, input_options: {})
+    form_controls(method, label_options) do
+      text_field(method, input_options)
     end
   end
 
@@ -31,7 +32,22 @@ class ApplicationFormBuilder < ActionView::Helpers::FormBuilder
     end
   end
 
+  def number_field(method, options = {})
+    super(method, options.merge(class: 'input'))
+  end
+
   def text_field(method, options = {})
     super(method, options.merge(class: 'input'))
+  end
+
+  private
+
+  def form_controls(method, label_options)
+    tag.div(class: 'field') do
+      label(method, label_options) +
+      tag.div(class: 'control') do
+        yield
+      end
+    end
   end
 end
