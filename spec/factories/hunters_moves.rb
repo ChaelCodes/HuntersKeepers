4e264b2aa328c34f302fa018e990d3de5a2cdb62
ChaelCodes <chaelcodes@gmail.com>
2020-11-22 10:32:55 -0500

    Annotate models, factories, and spec

--- spec/factories/hunters_moves.rb
+++ spec/factories/hunters_moves.rb
@@ -5,9 +5,22 @@
 # Table name: hunters_moves
 #
 #  id        :bigint           not null, primary key
+#  advanced  :boolean
+#  haven_id  :bigint
 #  hunter_id :bigint
 #  move_id   :bigint
-#  advanced  :boolean
+#
+# Indexes
+#
+#  index_hunters_moves_on_haven_id   (haven_id) WHERE (haven_id IS NOT NULL)
+#  index_hunters_moves_on_hunter_id  (hunter_id)
+#  index_hunters_moves_on_move_id    (move_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (haven_id => havens.id)
+#  fk_rails_...  (hunter_id => hunters.id)
+#  fk_rails_...  (move_id => moves.id)
 #
 FactoryBot.define do
   factory :hunters_move do
