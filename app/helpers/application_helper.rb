ab5a0d7bfb62981e284f95576347dbda4a0b7a2b
ChaelCodes <chaelcodes@gmail.com>
2020-12-12 14:30:39 -0500

    Rewrite page_buttons to be compliant with code climate.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,15 +4,23 @@
 module ApplicationHelper
   def show_page_buttons(object)
     tag.div(class: 'field has-addons') do
-      buttons = []
-      if policy(object).edit?
-        buttons << link_to(t('.edit'), [:edit, object], class: 'button')
-      end
-      buttons << link_to(t('.index'), object.class, class: 'button')
-      if policy(object).destroy?
-        buttons << link_to(t('.destroy'), object, method: :delete, class: 'button', data: { confirm: t('.confirm_destroy') })
-      end
-      safe_join(buttons.map { |button| tag.p(class: 'control') { button } })
+      safe_join(page_buttons(object).map { |button| tag.p(class: 'control') { button } })
     end
+  end
+
+  def page_buttons(object)
+    buttons = []
+    if policy(object).edit?
+      buttons << link_to(t('.edit'), [:edit, object], class: 'button')
+    end
+    buttons << link_to(t('.index'), object.class, class: 'button')
+    if policy(object).destroy?
+      buttons << destroy_button(object)
+    end
+    buttons
+  end
+
+  def destroy_button(object)
+    link_to(t('.destroy'), object, method: :delete, class: 'button', data: { confirm: t('.confirm_destroy') })
   end
 end
