732c753b17768afdd02b3a0aa26aebc2c4453980
ChaelCodes <chaelcodes@gmail.com>
2021-01-03 09:57:24 -0500

    Fixed model for form

--- app/helpers/hunter_backstories_helper.rb
+++ app/helpers/hunter_backstories_helper.rb
@@ -2,4 +2,13 @@ module HunterBackstoriesHelper
   def hunter_backstory_name(hunter_backstory)
     hunter_backstory.playbook.backstory&.dig(:name) || hunter_backstory.id
   end
+
+  # correct model for form
+  def hunter_backstory_form_model(hunter_backstory)
+    if hunter_backstory.persisted?
+      hunter_backstory
+    else
+      [hunter_backstory.hunter, hunter_backstory]
+    end
+  end
 end
