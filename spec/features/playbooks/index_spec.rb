6a21aff4a45fb2712d7812653ef47d6926772e40
ChaelCodes <chaelcodes@gmail.com>
2021-01-31 11:12:08 -0500

    fixup! fixup! Move old MiniTest playbook specs to RSpec feature tests.

--- spec/features/playbooks/index_spec.rb
+++ spec/features/playbooks/index_spec.rb
@@ -7,7 +7,7 @@ describe 'playbooks#index' do
     sign_in user
   end
 
-  context 'with 3 playbooks' do
+  context 'with a playbooks' do
     let!(:playbooks) { create :playbook, name: 'The Nameless' }
 
     it 'has a list of playbooks' do
@@ -23,11 +23,14 @@ describe 'playbooks#index' do
 
   context 'when admin' do
     let(:user) { create :user, :admin }
-    let!(:playbook) { create :playbook }
+    let(:playbook) { create :playbook }
 
-    it 'deletes a playbook' do
+    it 'deletes the selected playbook' do
+      Playbook.delete_all
+      playbook
       visit '/playbooks'
       expect { click_link 'Destroy' }.to change(Playbook, :count).by(-1)
+      expect { playbook.reload }.to raise_error(ActiveRecord::RecordNotFound)
       expect(page).to have_content('Playbook was successfully destroyed.')
     end
   end
