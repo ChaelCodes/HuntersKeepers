527e2fb2a7944d86163b45669bf51f10f776d3d9
ChaelCodes <chaelcodes@gmail.com>
2021-02-07 13:02:20 -0500

    Closer to a value object, but it won't persist in the DB

--- spec/models/playbook/config_type_spec.rb
+++ spec/models/playbook/config_type_spec.rb
@@ -3,29 +3,24 @@ require 'rails_helper'
 RSpec.describe Playbook::ConfigType, type: :model do
   let(:playbook_config_type) { build(:playbook_config_type) }
 
-  xit 'has a valid factory' do
+  it 'has a valid factory' do
     expect(playbook_config_type).to be_kind_of(Playbook::ConfigType)
   end
 
-  xdescribe '#serialize' do
+  describe '#serialize' do
     subject { playbook_config_type.serialize(playbook_config_type) }
 
     it { is_expected.to eq '{"backstory":{}}' }
   end
 
   context 'ensuring column storage' do
-    it 'stores config on an already saved playbook' do
-      playbook = create(:playbook)
-      playbook.config = { backstory: {
-                  name: "Fate",
-                  headings: [{name: "How you found out.",
-                              count: 1,
-                              choices: ["Nightmares and visions", "Some weirdo told you."] }]}
-    }
+    let(:playbook) { create(:playbook) }
+
+    it 'stores data in the playbook' do
+      playbook.config = { backstory: { name: 'The Name' } }
       playbook.save
       playbook.reload
-      expect(playbook.config.name).to eq "Fate"
-      expect(playbook.config.headings.first[:name]).to eq "How you found out."
+      expect(playbook.backstory[:name]).to eq 'The Name'
     end
   end
 end
