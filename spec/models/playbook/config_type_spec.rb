ea80c141651ef793cac6135b568ae45ef3845c6e
Corey Haines <coreyhaines@gmail.com>
2021-02-08 18:25:05 -0600

    Switch to using a specific type for backstory

--- spec/models/playbook/config_type_spec.rb
+++ spec/models/playbook/config_type_spec.rb
@@ -14,13 +14,18 @@ RSpec.describe Playbook::ConfigType, type: :model do
   end
 
   context 'ensuring column storage' do
-    let(:playbook) { create(:playbook) }
-
-    it 'stores data in the playbook' do
-      playbook.config = { backstory: { name: 'The Name' } }
+    it 'stores config on an already saved playbook' do
+      playbook = create(:playbook)
+      playbook.config = { backstory: {
+                  name: "Fate",
+                  headings: [{name: "How you found out.",
+                              count: 1,
+                              choices: ["Nightmares and visions", "Some weirdo told you."] }]}
+    }
       playbook.save
       playbook.reload
-      expect(playbook.backstory[:name]).to eq 'The Name'
+      expect(playbook.config.name).to eq "Fate"
+      expect(playbook.config.headings.first[:name]).to eq "How you found out."
     end
   end
 end
