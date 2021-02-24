bac88d9f9f9ca41ccfc3c9a317f99b4db8c5bbcc
Danny Ramos <danramos21@gmail.com>
2021-02-23 18:08:20 -0700

    AGAIN

--- app/models/playbook.rb
+++ app/models/playbook.rb
@@ -27,9 +27,9 @@ class Playbook < ApplicationRecord
   has_many :moves, dependent: :destroy
   has_many :ratings, dependent: :destroy
   validates :name, presence: true
-  
+
   default_scope { unarchived }
-  scope :unarchived, -> { where(archived_at: nil ) } 
+  scope :unarchived, -> { where(archived_at: nil) } 
 
   def archive!
     self.archived_at ||= Time.zone.now
