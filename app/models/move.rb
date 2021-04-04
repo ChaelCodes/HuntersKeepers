9293084ef06e6fc447c1a1683d31d9c85bba6a3f
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2021-04-04 17:06:55 +0000

    Fix query for Moves that include hunters_moves
    Using where excluded all HuntersMoves that already belonged to another
    hunter. Changing to a LEFT JOIN included all records regardless of
    hunter ownership, and still included hunters_move data.

--- app/models/move.rb
+++ app/models/move.rb
@@ -53,8 +53,11 @@ class Move < ApplicationRecord
       .where(hunters: { id: hunter_id })
   end
   scope :include_hunter_moves, ->(hunter_id) do # rubocop:disable Style/Lambda
+    query = %(LEFT JOIN hunters_moves on
+              hunters_moves.hunter_id = #{hunter_id}
+              AND moves.id = hunters_moves.move_id)
     includes(:hunters_moves)
-      .where(hunters_moves: { hunter_id: [hunter_id, nil] })
+      .joins(query)
   end
   scope :haven, -> { where(haven: true) }
 
