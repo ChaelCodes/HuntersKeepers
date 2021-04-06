e3ecb46515b6414253ca5efec19f9877f7d43a17
Troy Underwood <troyizzle@gmail.com>
2021-04-05 20:26:03 -0400

    Add delete links for hunter backstories

--- spec/features/hunter_backstories/show_spec.rb
+++ spec/features/hunter_backstories/show_spec.rb
@@ -35,4 +35,29 @@ describe 'hunter_backstories/:id' do
     click_link 'Back'
     expect(page).to have_content 'Hunter Backstories'
   end
+
+  describe 'page buttons' do
+    let(:have_index_link) { have_link I18n.t('hunter_backstories.show.index'), href: hunter_backstories_path }
+    let(:have_edit_link) { have_link I18n.t('hunter_backstories.show.edit'), href: edit_hunter_backstory_path(hunter_backstory) }
+    let(:have_delete_link) { have_link I18n.t('hunter_backstories.show.destroy'), href: hunter_backstory_path(hunter_backstory) }
+
+    before :each do
+      sign_in user
+      visit "/hunter_backstories/#{hunter_backstory.id}".dup
+    end
+
+    context 'when user is logged in' do
+      it { expect(page).to have_index_link }
+      it { expect(page).not_to have_edit_link }
+      it { expect(page).not_to have_delete_link }
+    end
+
+    context 'when admin is logged in' do
+      let(:user) { create :user, :admin }
+
+      it { expect(page).to have_index_link }
+      it { expect(page).to have_edit_link }
+      it { expect(page).to have_delete_link }
+    end
+  end
 end
